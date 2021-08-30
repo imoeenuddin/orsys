23de43d695c71c74b0e98d981ef1720e966a8e80
Mohammad Moin <mmoin@restore.com>
2021-08-31 03:41:42 +0500

    code format

--- assets/images/includes/head.php
+++ assets/images/includes/head.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="zxx">
+
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <meta name="description" content="Orbitor,business,company,agency,modern,bootstrap4,tech,software">
-  <meta name="author" content="themefisher.com">
+
 
   <title>Novena- Health & Care Medical template</title>
 
   <!-- Favicon -->
-  <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
+  <link rel="shortcut icon" type="image/x-icon" href="/assets/images/favicon.ico" />
 
   <!-- bootstrap.min css -->
   <link rel="stylesheet" href="../../plugins/bootstrap/css/bootstrap.min.css">
-  
+
   <!-- Icon Font Css -->
   <link rel="stylesheet" href="../../plugins/icofont/icofont.min.css">
   <!-- Slick Slider  CSS -->
@@ -20,6 +21,6 @@
   <link rel="stylesheet" href="../../plugins/slick-carousel/slick/slick-theme.css">
 
   <!-- Main Stylesheet -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="assets/css/style.css">
 
 </head>
